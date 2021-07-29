d32ef0b4794fada4af856c1fa9bff151a37fa20d
leogalvao289 <leogalvao289@gmail.com>
2021-07-29 18:43:42 +0200

    update

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -5,30 +5,31 @@ Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6 */
 
-    document.querySelectorAll('p').forEach(length <=6);
-     
-    
+ let paragraph = document.querySelectorAll('p')
+ console.log(paragraph.length <= 6)
+
 /*
     2. the first div element node
     --> should log the ".site-header" node */
 
+let siteheader = document.querySelector('.site-header')
+siteheader.style.background = 'blue'
 
-
-
+console.log(siteheader)
 
 /*    3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node */
 
+let jumbotron = document.querySelector('#jumbotron-text')
+jumbotron.style.background = 'green'
 
+console.log(jumbotron)
 
+/*  4. all the "p" elements of contained inside  the .primary-content element node
+    --> should log a list of nodes with a length of 3 */
 
- /*   4. all the "p" elements of contained inside  the .primary-content element node
-    --> should log a list of nodes with a length of 3 /*
-
-
-
-*/
-
+  let p =  document.querySelectorAll('p >.primary-content')
+  console.log(p.length <= 3)
 
 /*
 Task 2
@@ -37,14 +38,12 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
-
 /*
 Task 3
 =======
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
 
 /*
 Task 4
@@ -53,15 +52,12 @@ Task 4
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
 */
 
-
-
 /*
 Task 5
 ======
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
-
 
 /*
 Task 6
@@ -80,4 +76,3 @@ Create an array of 5 different colors.
 Using the same function in Task 3, every time the 'Change colour' button is clicked, the background color will be changed with the next color in the array.
 The next color when you are in the last color of the array will be the first color again.
 */
-
