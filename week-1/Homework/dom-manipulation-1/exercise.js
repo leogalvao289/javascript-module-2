17f94bfc3be294769448b644d11fcfdeccaaf444
leogalvao289 <leogalvao289@gmail.com>
2021-07-29 17:16:15 +0200

    update

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -3,16 +3,29 @@ Task 1
 =======
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
-    --> should log a list of nodes with a length of 6
+    --> should log a list of nodes with a length of 6 */
 
+    document.querySelectorAll('p').forEach(length <=6);
+     
+    
+/*
     2. the first div element node
-    --> should log the ".site-header" node
+    --> should log the ".site-header" node */
 
-    3. the element with id "jumbotron-text"
-    --> should log the "#jumbotron-text" node
 
-    4. all the "p" elements of contained inside  the .primary-content element node
-    --> should log a list of nodes with a length of 3
+
+
+
+/*    3. the element with id "jumbotron-text"
+    --> should log the "#jumbotron-text" node */
+
+
+
+
+ /*   4. all the "p" elements of contained inside  the .primary-content element node
+    --> should log a list of nodes with a length of 3 /*
+
+
 
 */
 
