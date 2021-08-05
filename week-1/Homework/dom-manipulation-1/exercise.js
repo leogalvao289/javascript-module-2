7e71e92d74d03543f1afa5de8d0652f9136272a7
leogalvao289 <leogalvao289@gmail.com>
2021-08-05 16:16:30 +0200

    update

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -7,31 +7,27 @@ Write JavaScript below that logs:
 
 let paragraph = document.querySelectorAll('p')
 
-console.log(paragraph)
+console.log('La longitud es igual a 6 ', paragraph.length === 6)
 
 /*
     2. the first div element node
     --> should log the ".site-header" node */
 
 let siteheader = document.querySelector('.site-header')
-siteheader.style.background = 'blue'
-
 console.log(siteheader)
 
 /*    3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node */
 
 let jumbotron = document.querySelector('#jumbotron-text')
-jumbotron.style.background = 'green'
-
 console.log(jumbotron)
 
 /*  4. all the "p" elements of contained inside  the .primary-content element node
     --> should log a list of nodes with a length of 3 */
-    
-    let matches = document.querySelectorAll(".primary-content > p");
 
-   console.log(matches)
+let matches = document.querySelectorAll('.primary-content  p')
+
+console.log(matches.length === 3)
 /*
 Task 2
 ======
@@ -44,8 +40,6 @@ button.addEventListener('click', mostrarAlerta)
 function mostrarAlerta () {
   alert('Thanks for visiting Bikes for Refugees!')
 }
-  
- 
 
 /*
 Task 3
@@ -53,33 +47,46 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-let but = document.getElementById('alertBtn')
-but.addEventListener('click', clicar)
-but.addEventListener('mouseenter', enter)
-but.addEventListener('mouseout', exit)
+const but = document.getElementById('bgrChangeBtn')
+but.addEventListener('click', event => {
+    clicar(event)
+})
 
-function clicar () {
-    but.style.background = 'red'
+function clicar (event, color) {
+    const arrColors = ['blue', 'orange' , 'red' , 'yellow']
+    event.preventDefault();
+    const body = document.querySelector('body');
+    const bodyColor = body.style.backgroundColor;
+    let colorIndex = arrColors.indexOf(bodyColor);
+
+    if (colorIndex === -1 || (colorIndex === arrColors.length -1)) {
+       body.style.backgroundColor = color || arrColors[0];
+    } else {
+        body.style.backgroundColor = color || arrColors[colorIndex + 1];
 }
-function exit () {
-    but.style.background = 'white'
+}
+/*function exit () {
+  body.style.backgroundColor = 'white'
 }
 function enter () {
-    but.style.background = 'red'
-}
+  body.style.backgroundcolor = 'red'
+}/*
 /*
 Task 4
 ======
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE” */
-let botao = document.getElementById ('addArticleBtn')
-botao.addEventListener('click', addPara)
 
-function addPara() {
-    let h = document.createElement("p");
-    let t = document.createTextNode("");
-    h.appendChild(t);
-    document.body.appendChild(h);
-  }
+const addTextBcn = document.querySelector('#addtextBtn') // llamo el boton con la Jquery
+const mainArticles = document.querySelector('#mainArticles') //llamo el sitio dondo va el texto
+addTextBcn.addEventListener('click', () => {
+  addP('hi everyone');
+})
+
+function addP (text) {
+  const para = document.createElement('p') // const para crear el elemento para insertar
+  para.textContent = text || 'lorem ipsum'  // tomara el valor por defecto 
+  mainArticles.appendChild(para)
+}
 
 /*
 Task 5
@@ -88,15 +95,15 @@ Task 5
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
 
-let largerLinksBtn = document.getElementById ('largerLinksBtn')
-largerLinksBtn.addEventListener('click' , fonte)
+const largerLinksBtn = document.querySelector('#largerLinksBtn') // Declaro el boton
+largerLinksBtn.addEventListener('click', function () {
+  // agrego la funcion con evento si dan click
+  const linksMenu = Array.from(document.querySelectorAll('a')) // convertir un nodeList en una matriz con Array.from
 
-function fonte(e){
-	let elemento = $(".facebook-link");
-	let fonte = elemento.css('font-size');
-	e == elemento.css("fontSize", parseInt(fonte) + 1);
-}
-	
+  linksMenu.forEach(linksElement => {
+    linksElement.style.fontSize = 25
+  })
+})
 
 /*
 Task 6
@@ -106,8 +113,14 @@ Using the same function in Task 4,
 When the 'Add' button is clicked, get the text inside the input field and create a new paragraph in the "LEARN MORE" section
 Also clear the text inside the input field
 */
+const addBtn = document.querySelector('#addArticleBtn')
+addBtn.addEventListener('click', ()=> {
+    const textInput = document.querySelector('input');
+    const inputValue = textInput.value;
 
-
+addP(inputValue)
+textInput.value = ""
+})
 
 /*
 Task 7
@@ -117,3 +130,5 @@ Create an array of 5 different colors.
 Using the same function in Task 3, every time the 'Change colour' button is clicked, the background color will be changed with the next color in the array.
 The next color when you are in the last color of the array will be the first color again.
 */
+
+const arrColors = ['blue', 'orange' , 'red' , 'yellow']
