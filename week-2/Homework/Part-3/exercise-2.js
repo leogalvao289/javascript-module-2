6f4a7b5fb7aaba0c819cb458adcb574780202295
leogalvao289 <leogalvao289@gmail.com>
2021-08-27 16:47:11 +0200

    up

--- week-2/Homework/Part-3/exercise-2.js
+++ week-2/Homework/Part-3/exercise-2.js
@@ -5,20 +5,19 @@ Define a method "makeFriend" to add a new friend to her list.
 */
 
 let person = {
-    name: "Alice",
-    friends: ["John", "Nina"],
-  };
-  
-  function makeFriend (newPerson) {
-    return person.push(newPerson)
+  name: 'Alice',
+  friends: ['John', 'Nina'],
+  makeFriend: function () {
+    return person.friends.push('Bob')
   }
+}
 
-  /*
+/*
   DO NOT EDIT ANYTHING BELOW THIS LINE
   */
-  
-  person.makeFriend("Bob");
-  
-  console.log(
-    `Expected result: 'John,Nina,Bob'. Actual result: ${person.friends}`
-  );
+
+person.makeFriend('Bob')
+
+console.log(
+  `Expected result: 'John,Nina,Bob'. Actual result: ${person.friends}`
+)
